0a7baa90a879815165c4bafaaa0fdd6ed81e992e
usachev-dev <usachev.dev@gmail.com>
2020-08-08 09:55:58 +0300

    looking good

--- server/src/pages/game.ts
+++ server/src/pages/game.ts
@@ -1,13 +1,15 @@
-import {Request, Response} from "express";
-import path from 'path';
-import pug from "pug"
+import { Request, Response } from "express";
+import path from "path";
+import pug from "pug";
 
-const template =  pug.compileFile(path.resolve(__dirname, '../templates', 'game.pug'));
+const template = pug.compileFile(
+  path.resolve(__dirname, "../templates", "game.pug")
+);
 
 export function GamePage(req: Request): string {
-    return render({});
+  return render({});
 }
 
 function render(args: {}): string {
-    return template(args);
+  return template(args);
 }
